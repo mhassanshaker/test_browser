69235f40abcf0f343702ab1681f05de0fc1e0ed9
mhassanshaker <85250473+mhassanshaker@users.noreply.github.com>
2021-07-14 01:32:48 +0400

    first state

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TestWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(628, 437);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(12, 455);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(628, 47);
+            this.btnMessage.TabIndex = 1;
+            this.btnMessage.Text = "Send Text To Text Area";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 514);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(string.Format("{0}/staticpage.html",Application.StartupPath));
+           
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            //get the main div HTML element by ID
+            var cke1ContentsElements = webBrowser1.Document.GetElementById("cke_1_contents");
+            //from main div select all HTML elements with the tag name "textarea"
+            var cke1TextareaList = cke1ContentsElements.GetElementsByTagName("textarea");
+            //from the selected list of items set value attribute for first element 
+            cke1TextareaList[0].SetAttribute("value", "Send This String To TextArea ");
+           
+        }
+    }
+}
